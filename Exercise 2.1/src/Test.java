9a6ab163494670942240c9c5fb245cb46bead2ca
Nevskiy Boris <nevskiy_by@mail.ru>
2020-09-09 21:34:07 +0300

    initial commit

---
+++ Exercise 2.1/src/Test.java
@@ -0,0 +1,11 @@
+
+public class Test {
+	public static  int number;
+	public static char symbol;
+	public static boolean bool;
+	public static String str = "еп"; 
+	
+	public static void main(String[] str) {
+		System.out.print("number:" + number + "\nsymbol:" + symbol + "\nboolean:" + bool + "\nString: " + str);
+	}
+}
